9590c80a376fecb2b3fb9dff5cfe7f77b5b1b99e
ashleydonohoe <ashleyrdonohoe@gmail.com>
2017-12-27 22:28:10 -0500

    have map displaying with google-map-react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 import './App.css';
 
 class App extends Component {
+  //  Code for adding map adapted from https://developers.google.com/maps/documentation/javascript/tutorial
+  // API KEY: AIzaSyAk2Ky0jfCnkQWb9M7xWm1wxlji3nvJJyQ&
   render() {
     return (
       <div className="container">
@@ -10,6 +13,9 @@ class App extends Component {
             <h1 className="text-center">Grocery Store Finder</h1>
           </div>
         </header>
+        <main id="map">
+          <GoogleMapReact defaultCenter={{ lat: 59.95, lng: 30.33}} defaultZoom={11}></GoogleMapReact>
+        </main>
       </div>
     );
   }
