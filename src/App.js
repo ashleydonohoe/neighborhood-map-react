7c322dd06a9736a10eaca2561a5671db12563156
ashleydonohoe <ashleyrdonohoe@gmail.com>
2017-12-27 21:17:44 -0500

    added header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="container">
+        <header className="row">
+          <div className="col-md-12">
+            <h1 className="text-center">Grocery Store Finder</h1>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
